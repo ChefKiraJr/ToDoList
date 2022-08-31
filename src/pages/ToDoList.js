36023e33fb735e6cfc6c29a12d4a6dc5ccf1e9c1
Rafi Naufal <rafinaufal34@gmail.com>
2022-08-31 22:36:48 +0700

    last changes

---
+++ src/pages/ToDoList.js
@@ -0,0 +1,144 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './toDoList.css';
+import { Input, InputGroup, InputRightAddon, Box } from '@chakra-ui/react';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+
+const ToDoList = () => {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState({
+    activity: '',
+    id: '',
+  });
+  const [isEdit, setIsEdit] = useState(false);
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:3004/toDoList');
+      setData(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleChange = (e) => {
+    let temp = { ...input };
+    temp[e.target.name] = e.target.value;
+    setInput(temp);
+  };
+  const handleSubmit = async () => {
+    try {
+      let body = { ...input };
+      if (isEdit) {
+        await axios.put(`http://localhost:3004/toDoList/${body.id}`, body);
+      } else {
+        body.id = new Date().getTime();
+        await axios.post('http://localhost:3004/toDoList', body);
+      }
+      fetchData();
+      let refresh = { activity: '', id: '' };
+      setIsEdit(false);
+      setInput(refresh);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3004/toDoList/${id}`);
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleEdit = (value) => {
+    setIsEdit(true);
+    let temp = { ...input };
+    console.log(value);
+    temp.activity = value.activity;
+    temp.id = value.id;
+    setInput(temp);
+  };
+  console.log(input);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="master-container">
+        <div className="todo-container">
+          <div className="todo-header">
+            <p>What's The Plan for Today?</p>
+          </div>
+          <div className="todo-input">
+            {isEdit ? (
+              <InputGroup>
+                <Input
+                  name="activity"
+                  value={input.activity}
+                  borderColor={'blueviolet'}
+                  onChange={(event) => handleChange(event)}
+                />
+                <InputRightAddon
+                  className="add-button"
+                  children="Edit Todo"
+                  onClick={handleSubmit}
+                  color="white"
+                  backgroundColor="blueviolet"
+                  borderColor="blueviolet"
+                />
+              </InputGroup>
+            ) : (
+              <InputGroup>
+                <Input
+                  name="activity"
+                  value={input.activity}
+                  borderColor={'blueviolet'}
+                  placeholder="Input your plan here!"
+                  onChange={(event) => handleChange(event)}
+                />
+                <InputRightAddon
+                  className="add-button"
+                  children="Add Todo"
+                  onClick={handleSubmit}
+                  color="white"
+                  backgroundColor="blueviolet"
+                  borderColor="blueviolet"
+                />
+              </InputGroup>
+            )}
+          </div>
+          <div className="todo-content__container">
+            {data.length > 0 &&
+              data.map((value, index) => {
+                return (
+                  <Box
+                    className="todo-content__main"
+                    backgroundColor={
+                      index === 0 || index % 4 === 0
+                        ? 'blue'
+                        : index % 2 === 0
+                        ? 'green'
+                        : 'magenta'
+                    }
+                  >
+                    <div className="todo-content__text">{value.activity}</div>
+                    <div className="todo-content__icon">
+                      <DeleteIcon
+                        className="delete-button"
+                        onClick={() => handleDelete(data[index].id)}
+                      />
+                      <EditIcon
+                        className="edit-button"
+                        onClick={() => handleEdit(value)}
+                      />
+                    </div>
+                  </Box>
+                );
+              })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ToDoList;
